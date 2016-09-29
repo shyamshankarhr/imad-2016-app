b3f5d19f3c2031f7d565dd00e09973ac031f4fea
shyamshankarhr <shyamshankarcern@gmail.com>
2016-09-29 07:23:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,11 +5,56 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var shyamContent = {
+    title: "Shyam Shankar H R",
+    heading: "Shyam Shankar H R",
+    img: "ui/shyam.jpg",
+    content:"Koottathil ettavum look.. Verum kidilam. Mwunthaanu.",
+};
+
+function createTemplate(data){
+    
+    var title=data.title;
+    var heading=data.heading;
+    var img=data.img;
+    var content=data.content;
+    var htmlTemplate=
+    `
+    <html>
+    <head>
+        <title>
+            ${title}
+        </title>
+        <meta name="viewport" content="width-device-width, initial-scale-1"/>
+        <link href="ui/style.css" rel="stylesheet"/>
+        </head>
+    <body style="font-family: sans-serif;">
+        <ul class=nav-options>
+            <li><a href="/">Home |</a></li>
+            <li><a href="/shyam"> Shyam |</a></li>
+            <li><a href="/feroz"> Feroz | </a></li>
+            <li><a href="/gautam"> Gautam | </a></li>
+            <li><a href="/nagesh"> Nagesh |</a></li>
+            <li><a href="/aravind"> Aravind |</a></li>
+            <li><a href="/barjun"> B Arjun</a></li>
+        </ul>
+       <h2 class="name-title">${heading}</h2>
+        <img src="${img}" class="photo">
+        <h4 class="descrip">Description</h4>
+        <p class="content">
+            ${content}
+        </p>
+    </body>
+    </html>
+    `;
+    return htmlTemplate;
+}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/shyam', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'shyam.html'));
+  res.send(createTemplate(shyamContent));
 });
 app.get('/aravind', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'aravind.html'));
