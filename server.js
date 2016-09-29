16e0cb66cfce9936ef07ce12322ec7393d4c0a57
shyamshankarhr <shyamshankarcern@gmail.com>
2016-09-29 05:52:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -11,6 +11,9 @@ app.get('/', function (req, res) {
 app.get('/shyam', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'shyam.html'));
 });
+app.get('/aravind', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'aravind.html'));
+});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
